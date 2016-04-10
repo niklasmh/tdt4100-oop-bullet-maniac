f44f07ac34ca577bb887824ee6ed2d3325d38b2f
niklasmh <nikasmh@hotmail.com>
2016-04-10 13:56:01 +0200

    Made main run the screen framework

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,20 +2,36 @@ package game;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by niklasmh on 10.04.16.
  */
 public class Main extends Application {
 
+    private HashMap<String, String> screens = new HashMap<>();
+
     @Override
     public void start(Stage stg) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        stg.setTitle("Bullet Maniac");
+        screens.put("menu", "menu.fxml");
+
+        ScreenController scrCtrl = new ScreenController();
+
+        for (Map.Entry<String, String> screen: screens.entrySet()) {
+            scrCtrl.loadScreen(screen.getKey(), screen.getValue());
+        }
+
+        scrCtrl.setScreen("menu");
+
+        Group root = new Group();
+        root.getChildren().addAll(scrCtrl);
         stg.setScene(new Scene(root, 300, 275));
+        stg.setTitle("Bullet Maniac");
         stg.show();
     }
 
