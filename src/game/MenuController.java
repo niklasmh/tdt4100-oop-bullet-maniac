3e0b67585a7daa7f7f246d877ff729556ecec5b9
niklasmh <nikasmh@hotmail.com>
2016-04-10 13:33:34 +0200

    Extending framework in MenuController

--- src/game/MenuController.java
+++ src/game/MenuController.java
@@ -1,4 +1,26 @@
 package game;
 
-public class MenuController {
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by niklasmh on 10.04.16.
+ */
+public class MenuController implements Initializable, CurrentScreen {
+    ScreenController scrCtrl;
+
+    @Override
+    public void initialize (URL url, ResourceBundle src) {
+        // Code to initialize at start of screen
+    }
+
+    /**
+     *  Sets the screen to screen controller.
+     *
+     * @param screen = screen controller
+     */
+    public void setScreen(ScreenController screen) {
+        this.scrCtrl = screen;
+    }
 }
