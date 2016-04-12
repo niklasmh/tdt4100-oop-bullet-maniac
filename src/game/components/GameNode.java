e7dea044c152795f3e20aed142bc50529917edcb
niklasmh <nikasmh@hotmail.com>
2016-04-12 22:21:45 +0200

    Added game components with polar points as core

---
+++ src/game/components/GameNode.java
@@ -0,0 +1,13 @@
+package game.components;
+
+import java.awt.Point;
+
+/**
+ * Created by niklasmh on 12.04.16.
+ */
+public class GameNode extends PolarPoint {
+
+    public GameNode (double x, double y) {
+        super(x, y);
+    }
+}
---
+++ src/game/components/PolarPoint.java
@@ -0,0 +1,37 @@
+package game.components;
+
+/**
+ * Created by niklasmh on 12.04.16.
+ *
+ * This class is needed as the game uses polar coordinates.
+ */
+public class PolarPoint {
+    protected double radius;
+    protected double angle;
+
+    public PolarPoint(double radius, double angle) {
+        this.radius = radius;
+        this.angle = angle;
+    }
+
+    protected void setPoint (double radius, double angle) {
+        this.radius = radius;
+        this.angle = angle;
+    }
+
+    protected void setRadius (double radius) {
+        this.radius = radius;
+    }
+
+    protected void setAngle (double angle) {
+        this.angle = angle;
+    }
+
+    protected double getRadius () {
+        return this.radius;
+    }
+
+    protected double getAngle () {
+        return this.angle;
+    }
+}
