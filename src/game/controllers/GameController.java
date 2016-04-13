105dd1ba80732c4be86d12d82aec1a194485ab77
niklasmh <nikasmh@hotmail.com>
2016-04-13 13:09:07 +0200

    Added drawing to canvas and basic player controls

--- src/game/controllers/GameController.java
+++ src/game/controllers/GameController.java
@@ -1,6 +1,8 @@
 package game.controllers;
 
 import game.CurrentScreen;
+import game.components.Player;
+import game.components.ViewPoint;
 import game.resources.Global;
 import game.ScreenController;
 
@@ -22,39 +24,104 @@ import java.util.ResourceBundle;
 public class GameController implements Initializable, CurrentScreen, Global {
     ScreenController scrCtrl;
     GraphicsContext ctx;
+    ViewPoint viewPoint;
     @FXML Canvas canvas;
     @FXML VBox gameMenu;
     @FXML VBox gamePlay;
     boolean showMenu;
+    double zoom = 1;
 
     @Override
     public void initialize (URL url, ResourceBundle src) {
         ctx = canvas.getGraphicsContext2D();
-
         showMenu(false);
+
+        viewPoint = new ViewPoint(100, 100);
+        Player player = new Player(100, 100);
 
         new AnimationTimer() {
             @Override
             public void handle (long time) {
                 if (scrCtrl.getScreen().equals("game") && !showMenu) {
 
+                    /**
+                     * Game logic and controllers.
+                     */
+                    if (isKeyDown("LEFT")) {
+                        player.increaseX(-1);
+                    }
+
+                    if (isKeyDown("RIGHT")) {
+                        player.increaseX(1);
+                    }
+
+                    if (isKeyDown("UP")) {
+                        player.increaseY(-1);
+                    }
+
+                    if (isKeyDown("DOWN")) {
+                        player.increaseY(1);
+                    }
+
+                    /**
+                     * First clearing the stage for the next frame.
+                     */
                     canvas.setWidth(windowSize.getX());
                     canvas.setHeight(windowSize.getY());
-                    System.out.println(windowSize.getX());
-
                     ctx.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
                     ctx.setFill(Color.GRAY);
                     ctx.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-                    ctx.setStroke(Color.LIME);
-                    ctx.moveTo(0, 0);
-                    ctx.lineTo(Math.random() * 200, Math.random() * 200);
-                    ctx.moveTo(windowSize.getX(), windowSize.getY());
-                    ctx.lineTo(Math.random() * 200, Math.random() * 200);
-                    ctx.stroke();
+                    /**
+                     * Drawing the background.
+                     */
+
+                    /**
+                     * Drawing the player.
+                     */
+                    ctx.strokeOval(vpx(player.getX()) - vps(20) / 2, vpy(player.getY()) - vps(20) / 2, vps(20), vps(20));
                 }
             }
         }.start();
+    }
+
+    /**
+     * Convert scaling to right scale relative to screen.
+     * VPX = View Point Scale
+     */
+    public double vps(double val) {
+        return val * this.zoom;
+    }
+
+    /**
+     * Convert all points relative to ViewPoint.
+     * VPX = View Point X
+     */
+    public double vpx (double x) {
+        return x - this.viewPoint.getX();
+    }
+
+    /**
+     * Convert all points relative to ViewPoint.
+     * VPX = View Point X
+     */
+    public double vpy (double y) {
+        return y - this.viewPoint.getY();
+    }
+
+    public boolean isKeyDown (String key) {
+        switch (key) {
+            case "LEFT":
+                return keyCodes.contains("LEFT") || keyCodes.contains("A");
+            case "RIGHT":
+                return keyCodes.contains("RIGHT") || keyCodes.contains("D");
+            case "UP":
+                return keyCodes.contains("UP") || keyCodes.contains("W");
+            case "DOWN":
+                return keyCodes.contains("DOWN") || keyCodes.contains("S");
+            default:
+                return false;
+        }
     }
 
     /**
