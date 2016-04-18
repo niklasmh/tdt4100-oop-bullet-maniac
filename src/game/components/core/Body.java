4e1a2d7390b7f7a50fab7a7dcc191f58f10c6b89
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-04-19 01:07:51 +0200

    Added line intersection formula

--- src/game/components/core/Body.java
+++ src/game/components/core/Body.java
@@ -33,4 +33,23 @@ public class Body extends GameNode {
         double c = x2 * y1 - y2 * x1;
         return Math.abs(a * px + b * py + c) / Math.sqrt(a * a + b * b);
     }
+
+    public double intersectionBetweenLines (double px1, double py1, double px2, double py2, double qx1, double qy1, double qx2, double qy2, int coord) {
+        double t = -1;
+
+        if (((qx2-qx1)*(py2-py1)-(px2-px1)*(qy2-qy1)) != 0) {
+            t = -(-(qy2-qy1)*(px1-qx1)+(qx2-qx1)*(py1-qy1))/((qx2-qx1)*(py2-py1)-(px2-px1)*(qy2-qy1));
+        }
+
+        if (t >= 0 || t <= 1) {
+
+            if (coord == 0) {
+                return px1 + t * (px2 - px1);
+            } else if (coord == 1) {
+                return py1 + t * (py2 - py1);
+            }
+        }
+
+        return Double.NaN;
+    }
 }
